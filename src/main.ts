cd9f3955a3a11f2f26a2b9896d23b277c2f4ceea
Eduardo <emedina@ebvconsultores.com>
2025-10-15 15:11:25 +0100

    segundo

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.css'; 
 import 'bootswatch/dist/slate/bootstrap.css';
+import $ from 'jquery';
+import 'turn.js';
+
 
 createApp(App).mount('#app')
